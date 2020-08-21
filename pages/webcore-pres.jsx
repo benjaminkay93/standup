674aa6a420780636f5e50bd2c3143cbada098e62
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-08-21 13:18:03 +0100

    name useage of toGo and gone

--- pages/webcore-pres.jsx
+++ pages/webcore-pres.jsx
@@ -105,8 +105,8 @@ const update = (current) => (
   <>Give your update: <StyledStrong>{current}</StyledStrong></>
 )
 
-const Page = ({ team, gone = [], toGo }) => {
-  const [state, setState] = useState({ toGo, gone })
+const Page = ({ team, initialGone = [], initialToGo }) => {
+  const [state, setState] = useState({ toGo: initialToGo, gone: initialGone })
   const [url, setUrl] = useState('')
 
   const startStandup = () => {
@@ -127,18 +127,18 @@ const Page = ({ team, gone = [], toGo }) => {
   }
 
   const toggleMember = (memberPosition) => () => {
-    let toGo
-    let gone
+    let updatedToGo
+    let updatedGone
 
     if (state.toGo.includes(memberPosition)) {
-      gone = [...state.gone, memberPosition]
-      toGo = state.toGo.filter(val => val !== memberPosition)
+      updatedGone = [...state.gone, memberPosition]
+      updatedToGo = state.toGo.filter(val => val !== memberPosition)
     } else {
-      toGo = [...state.toGo, memberPosition]
-      gone = state.gone.filter(val => val !== memberPosition)
+      updatedToGo = [...state.toGo, memberPosition]
+      updatedGone = state.gone.filter(val => val !== memberPosition)
     }
 
-    setState({ ...state, gone, toGo })
+    setState({ ...state, gone: updatedGone, toGo: updatedToGo })
   }
 
   if (typeof window !== 'undefined') {
@@ -201,9 +201,10 @@ const Page = ({ team, gone = [], toGo }) => {
 Page.getInitialProps = async (ctx) => {
   const goneString = ctx.query.gone || '[]'
   const goneObject = JSON.parse(goneString)
-  const gone = goneObject
-  const toGo = teamData.map((_, index) => index).filter((index) => !goneObject.includes(index))
-  return { team: teamData, gone, toGo }
+  const initialGone = goneObject
+  const initialToGo = teamData.map((_, index) => index).filter((index) => !goneObject.includes(index))
+
+  return { team: teamData, initialGone: initialGone, initialToGo: initialToGo }
 }
 
 export default Page
